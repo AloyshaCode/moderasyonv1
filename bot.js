a76a9a5d1f77305fe0b6da12a8c988be1b991305
aloyshacode-tr <82256670+aloyshacode-tr@users.noreply.github.com>
2021-04-10 10:13:54 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,178 @@
+const discord = require('discord.js');
+const fs = require('fs');
+const http = require('http');
+const db = require('quick.db');
+const moment = require('moment')
+const express = require('express');
+const ayarlar = require('./ayarlar.json');
+const app = express();
+app.get("/", (request, response) => {
+response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+
+//READY.JS
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', async () => {
+   client.appInfo = await client.fetchApplication();
+  setInterval( async () => {
+    client.appInfo = await client.fetchApplication();
+  }, 600);
+  
+ client.user.setActivity(`Aloysha BotList & Code Tarafından Hazırlanmıştır <3`, { type:'WATCHING' })
+  
+  console.log("")
+});
+
+const log = message => {
+  console.log(` ${message}`);
+};
+require('./util/eventLoader.js')(client);
+
+//READY.JS SON
+
+//KOMUT ALGILAYICI
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+           reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+//KOMUT ALGILAYICI SON
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+client.login(process.env.TOKEN)
+
+
+//-----------------------KOMUTLAR-----------------------\\
+
+//OTOROL
+
+client.on("guildMemberAdd", async member => {
+
+  let kanal = await db.fetch(`otorolkanal_${member.guild.id}`);
+  let rol = await db.fetch(`otorolrol_${member.guild.id}`);
+
+  if (!kanal) return;
+  if (!rol) return;
+
+  let user = client.users.cache.get(member.id);
+
+  client.channels.cache.get(kanal).send(
+  new Discord.MessageEmbed()
+  .setColor("BLUE")
+  .setTimestamp()
+  .setThumbnail(user.avatarURL({ dynamic: true, format: "png", size: 1024 }))
+  .setDescription(`**${member}** Adlı Kullanıcı Sunucuya Katıldı Rolu Başarıyla Verildi!`))
+  
+  member.roles.add(rol)
+});
+////////////////////////////////////////
+client.on("message", async message => {
+  if (!message.guild) return;
+
+  if (db.has(`sayac_${message.guild.id}`) === true) {
+    if (db.fetch(`sayac_${message.guild.id}`) <= message.guild.members.cache.size) {
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`Tebrikler ${message.guild.name}!`)
+        .setDescription(`Başarıyla \`${db.fetch(`sayac_${message.guild.id}`)}\` Sayaçtakı Ayarlı Üye Sayısına Ulaştık Sayaç Otomatik Kapandı!`)
+        .setColor("RANDOM");
+      message.channel.send(embed);
+      message.guild.owner.send(embed);
+      db.delete(`sayac_${message.guild.id}`);
+    }
+  }
+});
+client.on("guildMemberRemove", async member => {
+  const channel = db.fetch(`sKanal_${member.guild.id}`);
+  if (db.has(`sayac_${member.guild.id}`) == false) return;
+  if (db.has(`sKanal_${member.guild.id}`) == false) return;
+
+    member.guild.channels.cache.get(channel).send(`**${member.user.username}** Adlı Üye Sunucudan Ayrıldı <a:zil:829254934733652019>! \`${db.fetch(`sayac_${member.guild.id}`)}\` Üyeye Son == \`${db.fetch(`sayac_${member.guild.id}`) - member.guild.memberCount}\` Üye Kaldı!`);
+});
+client.on("guildMemberAdd", async member => {
+  const channel = db.fetch(`sKanal_${member.guild.id}`);
+  if (db.has(`sayac_${member.guild.id}`) == false) return;
+  if (db.has(`sKanal_${member.guild.id}`) == false) return;
+
+    member.guild.channels.cache.get(channel).send(`**${member.user.username}** Sunucuya Katıldı <a:zil:829254934733652019>! \`${db.fetch(`sayac_${member.guild.id}`)}\` Üyeye Son == \`${db.fetch(`sayac_${member.guild.id}`) - member.guild.memberCount}\` Üye Kaldı!`);
+});
+///////////////////////////////////////////////////////////////
